b3b3ce334e040d91a548128754eb3ba0adfb2903
mariamiqrar17 <mariam_iqrar_intern@netixsol.com>
2024-01-22 11:39:31 +0500

    change

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,23 +1,18 @@
-import { mongooseConnect } from "@/lib/mongoose";
-const stripe = require("stripe")(process.env.STRIPE_SK);
-import { buffer } from "micro";
-import { Order } from "@/models/Order";
+import {mongooseConnect} from "@/lib/mongoose";
+const stripe = require('stripe')(process.env.STRIPE_SK);
+import {buffer} from 'micro';
+import {Order} from "@/models/Order";
 
-const endpointSecret =
-  "whsec_10bd092d957cd82d299d94f732a7ea5e4dfd9774ccba5898dd96bbabc1b7c1bb";
+const endpointSecret = "whsec_ff00aa7edfb5728656e0cf6dc4dad139330f15de3a3d90acdf28d2037563eef5";
 
-export default async function handler(req, res) {
+export default async function handler(req,res) {
   await mongooseConnect();
-  const sig = req.headers["stripe-signature"];
+  const sig = req.headers['stripe-signature'];
 
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(
-      await buffer(req),
-      sig,
-      endpointSecret
-    );
+    event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret);
   } catch (err) {
     res.status(400).send(`Webhook Error: ${err.message}`);
     return;
@@ -25,25 +20,25 @@ export default async function handler(req, res) {
 
   // Handle the event
   switch (event.type) {
-    case "checkout.session.completed":
+    case 'checkout.session.completed':
       const data = event.data.object;
       const orderId = data.metadata.orderId;
-      const paid = data.payment_status === "paid";
+      const paid = data.payment_status === 'paid';
       if (orderId && paid) {
-        await Order.findByIdAndUpdate(orderId, {
-          paid: true,
-        });
+        await Order.findByIdAndUpdate(orderId,{
+          paid:true,
+        })
       }
       break;
     default:
       console.log(`Unhandled event type ${event.type}`);
   }
 
-  res.status(200).send("ok");
+  res.status(200).send('ok');
 }
 
 export const config = {
-  api: { bodyParser: false },
+  api: {bodyParser:false,}
 };
 
 // bright-thrift-cajole-lean
